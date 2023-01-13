89c6de2a502bca1c0c15d16f8c4cf131c1f1a58f
Vova <strikerxp1990@gmail.com>
2023-01-13 19:03:44 +0200

    first release (beta)

---
+++ debug.py
@@ -0,0 +1,10 @@
+import pygame
+
+pygame.init()
+font = pygame.font.Font(None, 30)
+
+def debug(info, y = 10, x = 10):
+    disp_surf = pygame.display.get_surface()
+    debug_surf = font.render(str(info), True, 'Red')
+    debug_rect = debug_surf.get_rect(topleft = (x,y))
+    disp_surf.blit(debug_surf, debug_rect)
